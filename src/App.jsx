acbbf53e79b3f33efc975f592f876a3e1d6c99b8
gianlourbano <gianlorenzourbano@gmail.com>
2024-10-21 10:13:14 +0200

    demonstrate that wgpu and wasm have different outputs

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,36 @@ import { useModel } from "./hooks/useModel";
 
 function App() {
     const [webgpuEnabled, setWebgpuEnabled] = useState(false);
-    const [optimized, setOptimized] = useState(false);
+    const [profileEnabled, setProfileEnabled] = useState(false);
 
     const { run } = useModel();
 
     const onRun = useCallback(async () => {
 
-        await run(webgpuEnabled, optimized);
+        await run(true, profileEnabled); // wgpu
+        await run(false, false); // cpu, profiling disabled
         
-    }, [run, webgpuEnabled, optimized]);
+    }, [run, webgpuEnabled, profileEnabled]);
 
     return (
         <main style={{ display: "flex", flexDirection: "column" }}>
-            <button onClick={onRun}>Run Demucs</button>
+            <button onClick={onRun}>Run Demucs (first wasm then gpu)</button>
             <div>
                 <input
                     id="webgpu"
                     type="checkbox"
+                    disabled
                     onChange={(e) => setWebgpuEnabled(e.target.checked)}
                 />
                 <label htmlFor="webgpu">Enable WebGPU</label>
+            </div>
+            <div>
                 <input
-                    id="optimized"
+                    id="profile"
                     type="checkbox"
-                    onChange={(e) => setOptimized(e.target.checked)}
+                    onChange={(e) => setProfileEnabled(e.target.checked)}
                 />
-                <label htmlFor="optimized">Use optimized model</label>
+                <label htmlFor="profile">Enable profiling</label>
             </div>
         </main>
     );
--- src/hooks/useModel.js
+++ src/hooks/useModel.js
@@ -18,12 +18,31 @@ const load = async (webgpu) => {
     return runtime;
 };
 
+const no_chunks = 1;
+const audioChunks = [];
+const specs = [];
+
+for (let i = 0; i < no_chunks; i++) {
+    audioChunks.push(new Float32Array(1 * 2 * 441000));
+    specs.push(new Float32Array(1 * 2 * 2048 * 431 * 2));
+}
+
+for (let i = 0; i < no_chunks; i++) {
+    // fill with random values
+    for (let j = 0; j < audioChunks[i].length; j++) {
+        audioChunks[i][j] = Math.random();
+    }
+    for (let j = 0; j < specs[i].length; j++) {
+        specs[i][j] = Math.random();
+    }
+}
+
 export const useModel = () => {
     return {
-        run: async (webgpu, optimized) => {
+        run: async (webgpu, profiling) => {
             const { InferenceSession, Tensor, env } = await load(webgpu);
 
-            const model = optimized ? "htdemucs_optimized.onnx" : "demucs.onnx";
+            const model = "demucs.onnx";
             console.log("Using model: ", model);
 
             const model_binary = await fetch(model);
@@ -32,35 +51,21 @@ export const useModel = () => {
             );
 
             console.time("onnx");
+            const startTime = performance.now();
 
             const session = await InferenceSession.create(model_uint8, {
                 executionProviders: webgpu ? ["webgpu"] : undefined,
-                enableProfiling: false,
+                //enableProfiling: false,
             });
 
             let wgpu_profile = "[\n";
-            env.webgpu.profiling = {
-                mode: "default",
-                ondata: (data) => {
-                    wgpu_profile += JSON.stringify(data) + ",\n";
-                },
-            };
-
-            // simulating 5 audio chunks with their corresponding spectrograms
-            let audioChunks = [];
-            let specs = [];
-            for (let i = 0; i < 6; i++) {
-                // push both mix and spec
-                audioChunks.push(new Float32Array(1 * 2 * 441000));
-                specs.push(new Float32Array(1 * 2 * 2048 * 431 * 2));
-
-                // fill with random values
-                for (let j = 0; j < audioChunks[i].length; j++) {
-                    audioChunks[i][j] = Math.random();
-                }
-                for (let j = 0; j < specs[i].length; j++) {
-                    specs[i][j] = Math.random();
-                }
+            if (profiling) {
+                env.webgpu.profiling = {
+                    mode: "default",
+                    ondata: (data) => {
+                        wgpu_profile += JSON.stringify(data) + ",\n";
+                    },
+                };
             }
 
             let inputs = [];
@@ -81,56 +86,101 @@ export const useModel = () => {
 
             for (let idx = 0; idx < inputs.length; idx++) {
                 console.time(`step onnx ${idx}`);
-                const output = await session.run(inputs[idx], {}); // error here
+                const output = await session.run(inputs[idx], {});
+                console.log(output);
                 console.timeEnd(`step onnx ${idx}`);
             }
 
-            session.endProfiling();
-
-            wgpu_profile = wgpu_profile.slice(0, -2) + "\n]";
-            const profileWgpu = JSON.parse(
-                wgpu_profile
-            );
-
-            const output = profileWgpu.map((x) => {
-                const time = x.endTime - x.startTime;
-                const inputs = x.inputsMetadata.map((y) => y.dims);
-                const outputs = x.outputsMetadata.map((y) => y.dims);
-                const name = x.kernelName;
-                const type = x.kernelType;
-
-                //time is in microseconds, converto to milliseconds
-                return { time: time / 1000, inputs, outputs, name, type };
-            });
-
-            const kernelMap = new Map();
-
-            output.forEach((x) => {
-                if (!kernelMap.has(x.type)) {
-                    kernelMap.set(x.type, []);
-                }
-                kernelMap.get(x.type).push(x);
-            });
-
-            const kernelStats = Array.from(kernelMap.entries()).map(
-                ([name, data]) => {
-                    const total = data.reduce((acc, x) => acc + x.time, 0);
-                    const count = data.length;
-                    const avg = total / count;
-
-                    return { name, total, count, avg };
-                }
-            );
-
-            const sortedKernelStats = kernelStats.sort((a, b) => b.avg - a.avg);
-
-            console.log(
-                JSON.stringify(sortedKernelStats.slice(0, 10), null, 2)
-            );
+            //session.endProfiling();
 
             await session.release();
 
             console.timeEnd("onnx");
+            const endTime = performance.now();
+
+            if (profiling) {
+                const totalOnnxTime = endTime - startTime;
+
+                wgpu_profile = wgpu_profile.slice(0, -2) + "\n]";
+                const profileWgpu = JSON.parse(wgpu_profile);
+
+                const output = profileWgpu.map((x) => {
+                    const time = x.endTime - x.startTime;
+                    const name = x.kernelName;
+                    const type = x.kernelType;
+
+                    //time is in microseconds, convert to milliseconds
+                    return { time: time / 1000, name, type };
+                });
+
+                const kernelMap = new Map();
+
+                output.forEach((x) => {
+                    if (!kernelMap.has(x.type)) {
+                        kernelMap.set(x.type, []);
+                    }
+                    kernelMap.get(x.type).push(x);
+                });
+
+                const kernelStats = Array.from(kernelMap.entries()).map(
+                    ([name, data]) => {
+                        const total = data.reduce((acc, x) => acc + x.time, 0);
+                        const count = data.length;
+                        const avg = total / count;
+                        const percentage = (total / totalOnnxTime / 1000) * 100;
+
+                        return {
+                            name,
+                            total: parseFloat(total.toFixed(2)),
+                            count,
+                            avg: parseFloat(avg.toFixed(2)),
+                            percentage: `${parseFloat(percentage.toFixed(2))}%`,
+                        };
+                    }
+                );
+
+                const sortedKernelStats = kernelStats.sort(
+                    (a, b) => b.total - a.total
+                );
+
+                const nodesMap = new Map();
+
+                output.forEach((x) => {
+                    if (!nodesMap.has(x.name)) {
+                        nodesMap.set(x.name, []);
+                    }
+                    nodesMap.get(x.name).push(x);
+                });
+
+                const nodesStats = Array.from(nodesMap.entries()).map(
+                    ([name, data]) => {
+                        const total = data.reduce((acc, x) => acc + x.time, 0);
+                        const count = data.length;
+                        const avg = total / count;
+                        const percentage = (total / totalOnnxTime / 1000) * 100;
+
+                        return {
+                            name,
+                            total: parseFloat(total.toFixed(2)),
+                            count,
+                            avg: parseFloat(avg.toFixed(2)),
+                            percentage: `${parseFloat(percentage.toFixed(2))}%`,
+                        };
+                    }
+                );
+
+                const nodesStatsSorted = nodesStats.sort(
+                    (a, b) => b.total - a.total
+                );
+
+                console.log(
+                    JSON.stringify(sortedKernelStats.slice(0, 10), null, 2)
+                );
+
+                console.log(
+                    JSON.stringify(nodesStatsSorted.slice(0, 10), null, 2)
+                );
+            }
         },
     };
 };
